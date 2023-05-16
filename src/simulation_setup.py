7256ea9ff519ed4acc9d6592e7d6ce6e1ff44075
Vinci Wu <wu.vinci00@gmail.com>
2023-05-16 10:53:51 -0400

    Created a separate file for the setup for the simulation

--- src/simulation_setup.py
+++ src/simulation_setup.py
@@ -2,9 +2,6 @@ from models.Planet import Planet
 from models.Star import Star
 from utils.Physics import Physics
 
-import matplotlib.pyplot as plt
-from matplotlib import animation
-
 # Celestial Bodies
 earth = Planet(5.972e24, 6.371e6)
 sun = Star(1.989e30, 6.957e8, 3.828e+29, 5773.15)
@@ -14,7 +11,6 @@ AU = 1.5e11  # meters
 # Constants for Earth Sun
 average_earth_sun_distance = 1.496e11
 earth_gravitational_constant = Physics.law_of_universial_gravitation(sun.mass, earth.mass, 1) # using radius during simulation calculation
-print(earth_gravitational_constant)
 earth_aphelion = 1.521e11 # Farthest Earth to Sun distance (1.017 Astronomical Unit)
 earth_velocity_aphelion = 29290
 earth_eccentricity = 0.01671
@@ -79,53 +75,3 @@ while time < 1 * 365 * Physics.days_in_secs:
 
     # Update delta time
     time += delta_time
-
-# Grab and animate the simulation
-fig, ax = plt.subplots(figsize=(10,10))
-ax.set_aspect('equal')
-ax.grid()
-
-# Earth object
-earth_color = "#2C7BB6" # hex code for greenish blue
-line_earth, = ax.plot([], [], '-g', lw=1, c='blue')
-point_earth, = ax.plot([earth_aphelion], [0], marker="o"
-                , markersize=6
-                , markeredgecolor=earth_color
-                , markerfacecolor=earth_color)
-text_earth = ax.text(earth_aphelion, 0,'Earth')
-
-# Sun object
-sun_color = "#F4F71C" # hex code for yellow-orange
-point_sun, = ax.plot([0], [0], marker="o"
-                , markersize=18
-                , markeredgecolor=sun_color
-                , markerfacecolor=sun_color)
-text_sun = ax.text(0, 0, 'Sun')
-
-# Celestial Body Tracks
-earth_x_data, earth_y_data = [], []
-sun_x_data, sun_y_data = [], []
-
-def update(i):
-    earth_x_data.append(earth_x_list[i])
-    earth_y_data.append(earth_y_list[i])
-    
-    line_earth.set_data(earth_x_data, earth_y_data)
-    point_earth.set_data(earth_x_list[i], earth_y_list[i])
-    text_earth.set_position((earth_x_list[i], earth_y_list[i]))
-
-    point_sun.set_data(sun_x_list[i], sun_y_list[i])
-    text_sun.set_position((sun_x_list[i], sun_y_list[i]))
-
-    ax.axis('equal')
-    ax.set_xlim(-2*earth_aphelion, 2*earth_aphelion)
-    ax.set_ylim(-2*earth_aphelion, 2*earth_aphelion)
-
-    return line_earth, point_sun, point_earth, text_earth, text_sun
-
-anim = animation.FuncAnimation(fig
-                                ,func=update
-                                ,frames=len(earth_x_list)
-                                ,interval=1
-                                ,blit=True)
-plt.show()
