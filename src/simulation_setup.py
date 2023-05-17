19a90081f669f4562d92c09ebf5ee41376e07c73
Vinci Wu <wu.vinci00@gmail.com>
2023-05-16 23:37:08 -0400

    Removed excess variables and used class attributes instead

--- src/simulation_setup.py
+++ src/simulation_setup.py
@@ -3,18 +3,24 @@ from models.Star import Star
 from utils.Physics import Physics
 
 # Celestial Bodies
-earth = Planet(5.972e24, 6.371e6)
 sun = Star(1.989e30, 6.957e8, 3.828e+29, 5773.15)
+earth = Planet(5.972e24, 6.371e6, 0.01671, 1.521e11, 29290, 1)
+mars = Planet(6.4169e23, 3.3962e6, 0.0935, 2.49261e8, 21971, 2)
 
 AU = 1.5e11  # meters
 
 # Constants for Earth Sun
-average_earth_sun_distance = 1.496e11
 earth_gravitational_constant = Physics.law_of_universial_gravitation(sun.mass, earth.mass, 1) # using radius during simulation calculation
-earth_aphelion = 1.521e11 # Farthest Earth to Sun distance (1.017 Astronomical Unit)
-earth_velocity_aphelion = 29290
-earth_eccentricity = 0.01671
-earth_perihelon = earth_aphelion * (1 - earth_eccentricity) # Closest Earth to Sun distance (0.983 AU)
+# earth_eccentricity = 0.01671
+# earth_aphelion = 1.521e11 # Farthest Earth to Sun distance (1.017 Astronomical Unit)
+# earth_velocity_aphelion = 29290 # meters / second (m/s)
+# earh_perihelon = earth_aphelion * (1 - earth_eccentricity) # Closest Earth to Sun distance (0.983 AU)
+
+# Constants for Mars Sun
+mars_gravitational_constant = Physics.law_of_universial_gravitation(sun.mass, mars.mass, 1)
+# mars_eccentricity = 0.0935
+# mars_aphelion = 2.49261e8
+# mars_velocity_aphelion = 21971
 
 # starting conditions
 # Sun
@@ -22,8 +28,8 @@ sun_x, sun_y, sun_z = 0, 0, 0
 sun_velocity_x, sun_velocity_y, sun_velocity_z = 0, 0, 0
 
 # Earth
-earth_x, earth_y, earth_z = earth_perihelon, 0, 0
-earth_velocity_x, earth_velocity_y, earth_velocity_z = 0, earth_velocity_aphelion, 0
+earth_x, earth_y, earth_z = earth.perihelion, 0, 0
+earth_velocity_x, earth_velocity_y, earth_velocity_z = 0, earth.aphelion_velocity, 0
 
 # Time
 time = 0
@@ -34,7 +40,7 @@ sun_x_list, sun_y_list, sun_z_list = [], [], []
 earth_x_list, earth_y_list, earth_z_list = [], [], []
 
 # Start simulation
-while time < 1 * 365 * Physics.days_in_secs:
+while time < 5 * 365 * Physics.days_in_secs:
     ###### The Earth #######
     # For Earth, compute G force on Earth
     radius_x, radius_y, radius_z = earth_x - sun_x, earth_y - sun_y, earth_z - sun_z
