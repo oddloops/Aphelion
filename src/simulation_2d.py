1aca44dd6f9327a6072ca036917f9caba74f1599
Vinci Wu <wu.vinci00@gmail.com>
2023-05-18 00:25:51 -0400

    Created and added Mercury planet object to the simulation

--- src/simulation_2d.py
+++ src/simulation_2d.py
@@ -8,6 +8,11 @@ fig, ax = plt.subplots(figsize=(10, 10))
 ax.set_aspect('equal')
 ax.grid()
 
+# Mercury object
+line_mercury, = ax.plot([], [], '-', lw=1, color="brown")
+point_mercury, = ax.plot([], [], marker="o", markersize=6, markeredgecolor="brown", markerfacecolor="brown")
+text_mercury = ax.text(mercury.aphelion, 0, 'Mercury')
+
 # Earth object
 earth_color = "#2C7BB6"  # hex code for greenish blue
 line_earth, = ax.plot([], [], '-', lw=1, color="blue")
@@ -24,18 +29,20 @@ sun_color = "#F4F71C"  # hex code for yellow-orange
 point_sun, = ax.plot([], [], marker="o", markersize=18, markeredgecolor=sun_color, markerfacecolor=sun_color)
 text_sun = ax.text(0, 0, 'Sun')
 
-# Celestial Body Tracks
-earth_x_data, earth_y_data = [], []
-mars_x_data, mars_y_data = [], []
-sun_x_data, sun_y_data = [], []
-
 def update(i):
+    mercury_x_data.append(mercury_x_list[i])
+    mercury_y_data.append(mercury_y_list[i])
+
     earth_x_data.append(earth_x_list[i])
     earth_y_data.append(earth_y_list[i])
 
     mars_x_data.append(mars_x_list[i])
     mars_y_data.append(mars_y_list[i])
     
+    line_mercury.set_data(mercury_x_data, mercury_y_data)
+    point_mercury.set_data(mercury_x_list[i:i+1], mercury_y_list[i:i+1])
+    text_mercury.set_position((mercury_x_list[i], mercury_y_list[i]))
+
     line_earth.set_data(earth_x_data, earth_y_data)
     point_earth.set_data(earth_x_list[i:i+1], earth_y_list[i:i+1])
     text_earth.set_position((earth_x_list[i], earth_y_list[i]))
@@ -51,7 +58,7 @@ def update(i):
     ax.set_xlim(-3 * AU, 3 * AU)
     ax.set_ylim(-3 * AU, 3 * AU)
 
-    return line_earth, point_sun, point_earth, line_mars, point_mars, text_earth, text_mars, text_sun
+    return line_mercury, line_earth, line_mars, point_sun, point_mercury, point_earth, point_mars, text_sun, text_mercury, text_earth, text_mars
 
 anim = animation.FuncAnimation(fig, func=update, frames=len(earth_x_list), interval=1, blit=True)
 plt.show()
--- src/simulation_setup.py
+++ src/simulation_setup.py
@@ -1,39 +1,36 @@
 from models.Planet import Planet
 from models.Star import Star
 from utils.Physics import Physics
-
+import math
 # Celestial Bodies
-sun = Star(1.989e30, 6.957e8, 3.828e+29, 5773.15)
-earth = Planet(5.972e24, 6.371e6, 0.01671, 1.521e11, 29290, 1)
-mars = Planet(6.4169e23, 3.3962e6, 0.0935, 2.49261e11, 21971, 2)
+sun = Star(1.989e30, 6.957e8, 3.828e29, 5773.15)
+mercury = Planet(3.30104e23, 2.4397e6, 0.20563593, 6.9818e10, 38860)
+earth = Planet(5.972e24, 6.371e6, 0.01671123, 1.521e11, 29290, 1)
+mars = Planet(6.4169e23, 3.3895e6, 0.0933941, 2.49261e11, 21971, 2)
 
 AU = 1.5e11  # meters
 
-# Constants for Earth Sun
+# Gravitational Constants
+mercury_gravitational_constant = Physics.law_of_universial_gravitation(sun.mass, mercury.mass, 1)
 earth_gravitational_constant = Physics.law_of_universial_gravitation(sun.mass, earth.mass, 1) # using radius during simulation calculation
-# earth_eccentricity = 0.01671
-# earth_aphelion = 1.521e11 # Farthest Earth to Sun distance (1.017 Astronomical Unit)
-# earth_velocity_aphelion = 29290 # meters / second (m/s)
-# earh_perihelon = earth_aphelion * (1 - earth_eccentricity) # Closest Earth to Sun distance (0.983 AU)
-
-# Constants for Mars Sun
 mars_gravitational_constant = Physics.law_of_universial_gravitation(sun.mass, mars.mass, 1)
-# mars_eccentricity = 0.0935
-# mars_aphelion = 2.49261e8
-# mars_velocity_aphelion = 21971
 
 # starting conditions
 # Sun
 sun_x, sun_y, sun_z = 0, 0, 0
 sun_velocity_x, sun_velocity_y, sun_velocity_z = 0, 0, 0
 
+# Mercury
+mercury_x, mercury_y, mercury_z = mercury.aphelion, 0, 0
+mercury_velocity_x, mercury_velocity_y, mercury_velocity_z = 0, mercury.min_orbit_velocity, 0
+
 # Earth
 earth_x, earth_y, earth_z = earth.aphelion, 0, 0
-earth_velocity_x, earth_velocity_y, earth_velocity_z = 0, earth.aphelion_velocity, 0
+earth_velocity_x, earth_velocity_y, earth_velocity_z = 0, earth.min_orbit_velocity, 0
 
 # Mars
 mars_x, mars_y, mars_z = mars.aphelion, 0, 0
-mars_velocity_x, mars_velocity_y, mars_velocity_z = 0, mars.aphelion_velocity, 0
+mars_velocity_x, mars_velocity_y, mars_velocity_z = 0, mars.min_orbit_velocity, 0
 
 # Time
 time = 0
@@ -41,11 +38,35 @@ delta_time = 1 * Physics.days_in_secs # frames move in this time
 
 # Positions of Celestial Bodies
 sun_x_list, sun_y_list, sun_z_list = [], [], []
+mercury_x_list, mercury_y_list, mercury_z_list = [], [], []
 earth_x_list, earth_y_list, earth_z_list = [], [], []
 mars_x_list, mars_y_list, mars_z_list = [], [], []
 
-# Start simulation
+# Simulation data
 while time < 5 * 365 * Physics.days_in_secs:
+    ###### Mercury ######
+    # Mercury G force
+    mercury_radius_x, mercury_radius_y, mercury_radius_z = mercury_x - sun_x, mercury_y - sun_y, mercury_z - sun_z
+    mercury_magnitude_vector_3 = (mercury_radius_x ** 2 + mercury_radius_y ** 2 + mercury_radius_z ** 2) ** 1.5
+    force_mercury_x = -mercury_gravitational_constant * mercury_radius_x / mercury_magnitude_vector_3
+    force_mercury_y = -mercury_gravitational_constant * mercury_radius_y / mercury_magnitude_vector_3
+    force_mercury_z = -mercury_gravitational_constant * mercury_radius_z / mercury_magnitude_vector_3
+
+    # Update quantities (F = ma -> a = F / m)
+    mercury_velocity_x += force_mercury_x * delta_time / mercury.mass
+    mercury_velocity_y += force_mercury_y * delta_time / mercury.mass
+    mercury_velocity_z += force_mercury_z * delta_time / mercury.mass
+
+    # Update position
+    mercury_x += mercury_velocity_x * delta_time
+    mercury_y += mercury_velocity_y * delta_time
+    mercury_z += mercury_velocity_z * delta_time
+
+    # Save position
+    mercury_x_list.append(mercury_x)
+    mercury_y_list.append(mercury_y)
+    mercury_z_list.append(mercury_z)
+
     ###### The Earth ######
     # For Earth, compute G force on Earth
     radius_x, radius_y, radius_z = earth_x - sun_x, earth_y - sun_y, earth_z - sun_z
@@ -93,9 +114,9 @@ while time < 5 * 365 * Physics.days_in_secs:
     mars_z_list.append(mars_z)
 
     ###### The Sun ######
-    sun_velocity_x += -(force_earth_x + force_mars_x) * delta_time / sun.mass
-    sun_velocity_y += -(force_earth_y + force_mars_y) * delta_time / sun.mass
-    sun_velocity_z += -(force_earth_z + force_mars_z) * delta_time / sun.mass
+    sun_velocity_x += -(force_mercury_x + force_earth_x + force_mars_x) * delta_time / sun.mass
+    sun_velocity_y += -(force_mercury_y + force_earth_y + force_mars_y) * delta_time / sun.mass
+    sun_velocity_z += -(force_mercury_z + force_earth_z + force_mars_z) * delta_time / sun.mass
 
     # Update Sun position
     sun_x += sun_velocity_x * delta_time
@@ -109,3 +130,9 @@ while time < 5 * 365 * Physics.days_in_secs:
 
     # Update delta time
     time += delta_time
+
+# Celestial Body Tracks for simulation plot
+mercury_x_data, mercury_y_data = [], []
+earth_x_data, earth_y_data = [], []
+mars_x_data, mars_y_data = [], []
+sun_x_data, sun_y_data = [], []
