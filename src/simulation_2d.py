2a14582f4483e5e61fd6dda3c990621a1f7ea002
Vinci Wu <wu.vinci00@gmail.com>
2023-05-18 21:48:28 -0400

    Added jupiter object for the simulation

--- src/simulation_2d.py
+++ src/simulation_2d.py
@@ -29,9 +29,14 @@ line_mars, = ax.plot([], [], '-', lw=1, color="red")
 point_mars, = ax.plot([], [], marker="o", markersize=4, markeredgecolor="red", markerfacecolor="red")
 text_mars = ax.text(mars.aphelion, 0, 'Mars')
 
+# Jupiter object
+line_jupiter, = ax.plot([], [], '-', lw=1, color="red")
+point_jupiter, = ax.plot([], [], marker="o", markersize=8, markeredgecolor="red", markerfacecolor="red")
+text_jupiter = ax.text(jupiter.aphelion, 0, 'Jupiter')
+
 # Sun object
 sun_color = "#F4F71C"  # hex code for yellow-orange
-point_sun, = ax.plot([], [], marker="o", markersize=18, markeredgecolor=sun_color, markerfacecolor=sun_color)
+point_sun, = ax.plot([], [], marker="o", markersize=16, markeredgecolor=sun_color, markerfacecolor=sun_color)
 text_sun = ax.text(0, 0, 'Sun')
 
 def update(i):
@@ -47,6 +52,9 @@ def update(i):
     mars_x_data.append(mars_x_list[i])
     mars_y_data.append(mars_y_list[i])
     
+    jupiter_x_data.append(jupiter_x_list[i])
+    jupiter_y_data.append(jupiter_y_list[i])
+
     line_mercury.set_data(mercury_x_data, mercury_y_data)
     point_mercury.set_data(mercury_x_list[i:i+1], mercury_y_list[i:i+1])
     text_mercury.set_position((mercury_x_list[i], mercury_y_list[i]))
@@ -63,16 +71,20 @@ def update(i):
     point_mars.set_data(mars_x_list[i:i+1], mars_y_list[i:i+1])
     text_mars.set_position((mars_x_list[i], mars_y_list[i]))
 
+    line_jupiter.set_data(jupiter_x_data, jupiter_y_data)
+    point_jupiter.set_data(jupiter_x_list[i:i+1], jupiter_y_list[i:i+1])
+    text_jupiter.set_position((jupiter_x_list[i], jupiter_y_list[i]))
+
     point_sun.set_data(sun_x_list[i:i+1], sun_y_list[i:i+1])
     text_sun.set_position((sun_x_list[i], sun_y_list[i]))
 
     ax.axis('equal')
-    ax.set_xlim(-3 * AU, 3 * AU)
-    ax.set_ylim(-3 * AU, 3 * AU)
+    ax.set_xlim(-6 * AU, 6 * AU)
+    ax.set_ylim(-6 * AU, 6 * AU)
 
-    return line_mercury, line_venus, line_earth, line_mars,\
-           point_sun, point_mercury, point_venus, point_earth, point_mars, \
-           text_sun, text_mercury, text_venus, text_earth, text_mars
+    return line_mercury, line_venus, line_earth, line_mars, line_jupiter, \
+           point_sun, point_mercury, point_venus, point_earth, point_mars, point_jupiter, \
+           text_sun, text_mercury, text_venus, text_earth, text_mars, text_jupiter
 
 anim = animation.FuncAnimation(fig, func=update, frames=len(earth_x_list), interval=1, blit=True)
 plt.show()
--- src/simulation_setup.py
+++ src/simulation_setup.py
@@ -8,6 +8,10 @@ mercury = Planet(3.30104e23, 2.4397e6, 0.20563593, 6.9818e10, 38860)
 venus = Planet(4.8673e24, 6.0518e6, 0.00677672, 1.08941e11, 34780)
 earth = Planet(5.9722e24, 6.371e6, 0.01671123, 1.521e11, 29290, 1)
 mars = Planet(6.4169e23, 3.3895e6, 0.0933941, 2.49261e11, 21971, 2)
+jupiter = Planet(1.89813e27, 6.9911e7, 0.04838624, 8.16363e11, 12440, 95, 3)
+saturn = Planet(5.6832e26, 5.8232e7, 0.05386179, 1.506527e9, 9140, 83, 7)
+# uranus = Planet()
+# neptune = Planet()
 
 AU = 1.5e11  # meters
 
@@ -16,6 +20,7 @@ mercury_gravitational_constant = Physics.law_of_universial_gravitation(sun.mass,
 venus_gravitational_constant = Physics.law_of_universial_gravitation(sun.mass, venus.mass)
 earth_gravitational_constant = Physics.law_of_universial_gravitation(sun.mass, earth.mass) # using radius during simulation calculation
 mars_gravitational_constant = Physics.law_of_universial_gravitation(sun.mass, mars.mass)
+jupiter_gravitational_constant = Physics.law_of_universial_gravitation(sun.mass, jupiter.mass)
 
 # starting conditions
 # Sun
@@ -38,6 +43,10 @@ earth_velocity_x, earth_velocity_y, earth_velocity_z = 0, earth.min_orbit_veloci
 mars_x, mars_y, mars_z = mars.aphelion, 0, 0
 mars_velocity_x, mars_velocity_y, mars_velocity_z = 0, mars.min_orbit_velocity, 0
 
+# Jupiter
+jupiter_x, jupiter_y, jupiter_z = jupiter.aphelion, 0, 0
+jupiter_velocity_x, jupiter_velocity_y, jupiter_velocity_z = 0, jupiter.min_orbit_velocity, 0
+
 # Time
 time = 0
 delta_time = 1 * Physics.days_in_secs # frames move in this time
@@ -48,9 +57,10 @@ mercury_x_list, mercury_y_list, mercury_z_list = [], [], []
 venus_x_list, venus_y_list, venus_z_list = [], [], []
 earth_x_list, earth_y_list, earth_z_list = [], [], []
 mars_x_list, mars_y_list, mars_z_list = [], [], []
+jupiter_x_list, jupiter_y_list, jupiter_z_list = [], [], []
 
 # Simulation data
-while time < 5 * 365 * Physics.days_in_secs:
+while time < 15 * 365 * Physics.days_in_secs:
     ###### Mercury ######
     # Mercury G force
     mercury_radius_x, mercury_radius_y, mercury_radius_z = mercury_x - sun_x, mercury_y - sun_y, mercury_z - sun_z
@@ -121,7 +131,7 @@ while time < 5 * 365 * Physics.days_in_secs:
     earth_z_list.append(earth_z)
 
     ###### Mars ######
-    # Compute G force on Earth
+    # Mars G force
     mars_radius_x, mars_radius_y, mars_radius_z = mars_x - sun_x, mars_y - sun_y, mars_z - sun_z
     mars_magnitude_vector_3 = (mars_radius_x ** 2 + mars_radius_y ** 2 + mars_radius_z ** 2) ** 1.5
     force_mars_x = -mars_gravitational_constant * mars_radius_x / mars_magnitude_vector_3
@@ -143,10 +153,33 @@ while time < 5 * 365 * Physics.days_in_secs:
     mars_y_list.append(mars_y)
     mars_z_list.append(mars_z)
 
+    ###### Jupiter ######
+    # Jupiter G force
+    jupiter_radius_x, jupiter_radius_y, jupiter_radius_z = jupiter_x - sun_x, jupiter_y - sun_y, jupiter_z - sun_z
+    jupiter_magnitude_vector_3 = (jupiter_radius_x ** 2 + jupiter_radius_y ** 2 + jupiter_radius_z ** 2) ** 1.5
+    force_jupiter_x = -jupiter_gravitational_constant * jupiter_radius_x / jupiter_magnitude_vector_3
+    force_jupiter_y = -jupiter_gravitational_constant * jupiter_radius_y / jupiter_magnitude_vector_3
+    force_jupiter_z = -jupiter_gravitational_constant * jupiter_radius_z / jupiter_magnitude_vector_3
+
+    # Update quantities (F = ma -> a = F / m)
+    jupiter_velocity_x += force_jupiter_x * delta_time / jupiter.mass
+    jupiter_velocity_y += force_jupiter_y * delta_time / jupiter.mass
+    jupiter_velocity_z += force_jupiter_z * delta_time / jupiter.mass
+
+    # Update Earth position
+    jupiter_x += jupiter_velocity_x * delta_time
+    jupiter_y += jupiter_velocity_y * delta_time
+    jupiter_z += jupiter_velocity_z * delta_time
+
+    # Save the Earth position
+    jupiter_x_list.append(jupiter_x)
+    jupiter_y_list.append(jupiter_y)
+    jupiter_z_list.append(jupiter_z)
+
     ###### The Sun ######
-    sun_velocity_x += -(force_mercury_x + force_earth_x + force_mars_x) * delta_time / sun.mass
-    sun_velocity_y += -(force_mercury_y + force_earth_y + force_mars_y) * delta_time / sun.mass
-    sun_velocity_z += -(force_mercury_z + force_earth_z + force_mars_z) * delta_time / sun.mass
+    sun_velocity_x += -(force_mercury_x + force_venus_x + force_earth_x + force_mars_x + force_jupiter_x) * delta_time / sun.mass
+    sun_velocity_y += -(force_mercury_y + force_venus_y + force_earth_y + force_mars_y + force_jupiter_y) * delta_time / sun.mass
+    sun_velocity_z += -(force_mercury_z + force_venus_z + force_earth_z + force_mars_z + force_jupiter_z) * delta_time / sun.mass
 
     # Update Sun position
     sun_x += sun_velocity_x * delta_time
@@ -166,4 +199,5 @@ mercury_x_data, mercury_y_data = [], []
 venus_x_data, venus_y_data = [], []
 earth_x_data, earth_y_data = [], []
 mars_x_data, mars_y_data = [], []
+jupiter_x_data, jupiter_y_data = [], []
 sun_x_data, sun_y_data = [], []
