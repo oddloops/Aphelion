dd37c1a34e469c11294ecdcf803acdeb07a4e249
Vinci Wu <wu.vinci00@gmail.com>
2023-05-19 09:26:37 -0400

    Added colors most associated with each planet and added neptune object

--- src/simulation_2d.py
+++ src/simulation_2d.py
@@ -4,18 +4,20 @@ from matplotlib import animation
 from simulation_setup import *
 
 # Grab and animate the simulation
-fig, ax = plt.subplots(figsize=(10, 10))
+fig, ax = plt.subplots(figsize=(12, 8))
 ax.set_aspect('equal')
 ax.grid()
 
 # Mercury object
-line_mercury, = ax.plot([], [], '-', lw=1, color="brown")
-point_mercury, = ax.plot([], [], marker="o", markersize=3, markeredgecolor="brown", markerfacecolor="brown")
+mercury_color = "#808080"
+line_mercury, = ax.plot([], [], '-', lw=1, color=mercury_color)
+point_mercury, = ax.plot([], [], marker="o", markersize=3, markeredgecolor=mercury_color, markerfacecolor=mercury_color)
 text_mercury = ax.text(mercury.aphelion, 0, 'Mercury')
 
 # Venus object
-line_venus, = ax.plot([], [], '-', lw=1, color="pink")
-point_venus, = ax.plot([], [], marker="o", markersize=5, markeredgecolor="pink", markerfacecolor="pink")
+venus_color = "#DDBA7D"
+line_venus, = ax.plot([], [], '-', lw=1, color=venus_color)
+point_venus, = ax.plot([], [], marker="o", markersize=5, markeredgecolor=venus_color, markerfacecolor=venus_color)
 text_venus = ax.text(venus.aphelion, 0, 'Venus')
 
 # Earth object
@@ -25,19 +27,34 @@ point_earth, = ax.plot([], [], marker="o", markersize=6, markeredgecolor=earth_c
 text_earth = ax.text(earth.aphelion, 0, 'Earth')
 
 # Mars object
-line_mars, = ax.plot([], [], '-', lw=1, color="red")
-point_mars, = ax.plot([], [], marker="o", markersize=4, markeredgecolor="red", markerfacecolor="red")
+mars_color = "#FF0000"
+line_mars, = ax.plot([], [], '-', lw=1, color=mars_color)
+point_mars, = ax.plot([], [], marker="o", markersize=4, markeredgecolor=mars_color, markerfacecolor=mars_color)
 text_mars = ax.text(mars.aphelion, 0, 'Mars')
 
 # Jupiter object
-line_jupiter, = ax.plot([], [], '-', lw=1, color="red")
-point_jupiter, = ax.plot([], [], marker="o", markersize=8, markeredgecolor="red", markerfacecolor="red")
+jupiter_color = "#FF4500"
+line_jupiter, = ax.plot([], [], '-', lw=1, color=jupiter_color)
+point_jupiter, = ax.plot([], [], marker="o", markersize=8, markeredgecolor=jupiter_color, markerfacecolor=jupiter_color)
 text_jupiter = ax.text(jupiter.aphelion, 0, 'Jupiter')
 
 # Saturn object
-line_saturn, = ax.plot([], [], '-', lw=1, color="purple")
-point_saturn, = ax.plot([], [], marker="o", markersize=8, markeredgecolor="purple", markerfacecolor="purple")
+saturn_color = "#FFD700"
+line_saturn, = ax.plot([], [], '-', lw=1, color=saturn_color)
+point_saturn, = ax.plot([], [], marker="o", markersize=8, markeredgecolor=saturn_color, markerfacecolor=saturn_color)
 text_saturn = ax.text(saturn.aphelion, 0, 'Saturn')
+
+# Uranus object
+uranus_color = "#00FFFF"
+line_uranus, = ax.plot([], [], '-', lw=1, color=uranus_color)
+point_uranus, = ax.plot([], [], marker="o", markersize=8, markeredgecolor=uranus_color, markerfacecolor=uranus_color)
+text_uranus = ax.text(uranus.aphelion, 0, 'Uranus')
+
+# Neptune object
+neptune_color = "#0000FF"
+line_neptune, = ax.plot([], [], '-', lw=1, color=neptune_color)
+point_neptune, = ax.plot([], [], marker="o", markersize=8, markeredgecolor=neptune_color, markerfacecolor=neptune_color)
+text_neptune = ax.text(neptune.aphelion, 0, 'Neptune')
 
 # Sun object
 sun_color = "#F4F71C"  # hex code for yellow-orange
@@ -63,6 +80,12 @@ def update(i):
     saturn_x_data.append(saturn_x_list[i])
     saturn_y_data.append(saturn_y_list[i])
 
+    uranus_x_data.append(uranus_x_list[i])
+    uranus_y_data.append(uranus_y_list[i])
+
+    neptune_x_data.append(neptune_x_list[i])
+    neptune_y_data.append(neptune_y_list[i])
+
     line_mercury.set_data(mercury_x_data, mercury_y_data)
     point_mercury.set_data(mercury_x_list[i:i+1], mercury_y_list[i:i+1])
     text_mercury.set_position((mercury_x_list[i], mercury_y_list[i]))
@@ -87,16 +110,24 @@ def update(i):
     point_saturn.set_data(saturn_x_list[i:i+1], saturn_y_list[i:i+1])
     text_saturn.set_position((saturn_x_list[i], saturn_y_list[i]))
 
+    line_uranus.set_data(uranus_x_data, uranus_y_data)
+    point_uranus.set_data(uranus_x_list[i:i+1], uranus_y_list[i:i+1])
+    text_uranus.set_position((uranus_x_list[i], uranus_y_list[i]))
+
+    line_neptune.set_data(neptune_x_data, neptune_y_data)
+    point_neptune.set_data(neptune_x_list[i:i+1], neptune_y_list[i:i+1])
+    text_neptune.set_position((neptune_x_list[i], neptune_y_list[i]))
+
     point_sun.set_data(sun_x_list[i:i+1], sun_y_list[i:i+1])
     text_sun.set_position((sun_x_list[i], sun_y_list[i]))
 
     ax.axis('equal')
-    ax.set_xlim(-12 * AU, 12 * AU)
-    ax.set_ylim(-12 * AU, 12 * AU)
+    ax.set_xlim(-31 * AU, 31 * AU)
+    ax.set_ylim(-31 * AU, 31 * AU)
 
-    return line_mercury, line_venus, line_earth, line_mars, line_jupiter, line_saturn, \
-           point_sun, point_mercury, point_venus, point_earth, point_mars, point_jupiter, point_saturn, \
-           text_sun, text_mercury, text_venus, text_earth, text_mars, text_jupiter, text_saturn
+    return line_mercury, line_venus, line_earth, line_mars, line_jupiter, line_saturn, line_uranus, line_neptune, \
+           point_sun, point_mercury, point_venus, point_earth, point_mars, point_jupiter, point_saturn, point_uranus, point_neptune, \
+           text_sun, text_mercury, text_venus, text_earth, text_mars, text_jupiter, text_saturn, text_uranus, text_neptune
 
 anim = animation.FuncAnimation(fig, func=update, frames=len(earth_x_list), interval=1, blit=True)
 plt.show()
